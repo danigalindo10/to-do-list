20456800670ce9907afb784bda0580294f9598dd
Daniel Galindo <josedaniel.galindo@hotmail.com>
2022-07-13 14:44:04 -0500

    Create Add and Remove function

---
+++ src/modules/add-task.js
@@ -0,0 +1,30 @@
+import Task from './task.js';
+import createList from './create-list.js';
+import getData from './get-data.js';
+
+// Selectors
+const input = document.getElementById('new-task');
+const button = document.getElementById('add-task');
+const toDoTasks = getData();
+const alert = document.getElementById('alert');
+
+const addTaskEvent = () => {
+  button.addEventListener('click', () => {
+    if (input.value) {
+      const task = new Task(input.value, toDoTasks.lenght + 1);
+      createList(task);
+      toDoTasks.push(task);
+      // Local Storage
+      localStorage.setItem('toDoData', JSON.stringify(toDoTasks));
+      input.value = '';
+    } else {
+      alert.innerHTML = 'Empty field';
+      alert.style.display = 'block';
+      setTimeout(() => {
+        alert.style.display = 'none';
+      }, 100);
+    }
+  });
+};
+
+export default addTaskEvent;
